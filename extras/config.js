0ad9a40a8ab4c56f0cb8dc7c39d5f93b1e13d283
Admin <arlu@umich.edu>
2018-03-30 16:03:22 -0400

    Update ip

--- extras/config.js
+++ extras/config.js
@@ -1,3 +1,3 @@
 // arboretum ip address
-// const ARB_IP = "http://35.2.239.53:3000"; // SI
-const ARB_IP = "http://35.3.40.130:3000"; // BBB
+const ARB_IP = "http://35.2.239.53:3000"; // SI
+// const ARB_IP = "http://35.3.40.130:3000"; // BBB
