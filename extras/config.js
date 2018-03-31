15016a1bc14c9e35ff616d72cab208be6b0d7239
Admin <arlu@umich.edu>
2018-03-31 00:24:48 -0400

    Update sites / ip

--- extras/config.js
+++ extras/config.js
@@ -1,3 +1,4 @@
 // arboretum ip address
-const ARB_IP = "http://35.2.239.53:3000"; // SI
+// const ARB_IP = "http://35.2.239.53:3000"; // SI
 // const ARB_IP = "http://35.3.40.130:3000"; // BBB
+const ARB_IP = "http://10.0.0.28:3000"; // home
